fa69fd051ddbf540d061dcb07894309ded4c0449
Graham Goudeau <graham.goudeau@scorm.com>
2015-08-10 22:35:51 -0500

    Refactored, commented

--- search.js
+++ search.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('submit')
-        .addEventListener('click', function (mouseEvt) {
-            var searchTerms = mouseEvt.target.form.elements.search.value,
-                url = HtmlEncode('https://www.google.com/search?q=site:stackoverflow.com ' + searchTerms);
-            chrome.tabs.create({'url': url}, function () {
-            });
+    var submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', function (mouseEvt) {
+        var searchTerms = mouseEvt.target.form.elements.search.value,
+            url = HtmlEncode('https://www.google.com/search?q=site:stackoverflow.com ' + searchTerms);
+
+        // create a new tab with the search that the user entered
+        chrome.tabs.create({'url': url}, function () {
         });
+    });
 });
 
 // http://stackoverflow.com/questions/784586/convert-special-characters-to-html-in-javascript
+// encode the search string the user entered as HTML so we can turn it into a search
 function HtmlEncode(str) {
     var element = document.createElement("div");
     element.innerText = element.textContent = str;
